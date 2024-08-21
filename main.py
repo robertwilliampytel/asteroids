881319456388fe874bd5332caf6efae6ed661a37
robertwilliampytel <robertwilliampytel@gmail.com>
2024-08-20 23:13:28 -0500

    added game window to main.py

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ from   constants import *
 
 
 def main():
+
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color_black = pygame.Color(0, 0, 0)
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color_black)
+
+        pygame.display.flip()
+
+
 
 
 if __name__ == "__main__":
